00745b157b21915e6f026c1c1241169c23155911
randyaajr <randyaajrsp@gmail.com>
2020-12-23 00:45:05 +0000

    Bind data from db to accordion

--- static/js/script.js
+++ static/js/script.js
@@ -3,5 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
     let collapsibles = document.querySelectorAll(".collapsible");
     let collapsiblesInstance = M.Collapsible.init(collapsibles);
+    let tooltips = document.querySelectorAll(".tooltipped");
+    let tooltipsInstance = M.Tooltip.init(tooltips);
 });
 
